c8663dfa656274c47cd9706d26ada54fe2fcbe06
Tarun Boricha <tarunboricha2092001@gmail.com>
2024-04-05 09:15:20 +0530

    commit to deploy

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent{
+  profilePhoto: string = "https://lh3.googleusercontent.com/pw/ABLVV84a5nti7mNDTll6qFgqjVuFyuKcbIkUzaTWlO2D7l41InLPcU95ATP-rpnNtMS5m058ZhwyLqdLsNhrfwPD64R_mdA6YZmF4TlLGSERSAtTnOHUwxFYhTJ7_H67Oz6daqKkM4BGQ3EBBOOl84T6NMXHzA=w620-h827-s-no-gm?authuser=0";
 
+  imageLoaded(event:any) {
+    event.target.classList.add('loaded');
+  }
 }
